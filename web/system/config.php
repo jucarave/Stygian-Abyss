188bd6911d9d282ab30c340f148617136a95048f
slash <slash@slashware.net>
2015-03-25 23:36:10 -0500

    v1.2

--- web/system/config.php
+++ web/system/config.php
@@ -1,6 +1,6 @@
 <?php
-	$version = "A0.2";
-	$environment = "LOCAL";
+	$version = "1.2";
+	$environment = "SERVER";
 	$contextPath = "/stygian/";
 	
 	if ($environment == "SERVER"){
@@ -8,5 +8,7 @@
 		
 		if ($_SERVER['HTTP_HOST'] == "games.jucarave.net" || $_SERVER['HTTP_HOST'] == "games.jucarave.com")
 			$contextPath = "/stygian/";
+		if ($_SERVER['HTTP_HOST'] == "slashie.net")
+			$contextPath = "/stygian/play/";
 	} 
 ?>
